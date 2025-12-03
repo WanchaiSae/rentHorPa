1874184f8a5fe701b4f3f4a049f24117ee170fac
WanchaiSae <wanchai.sae@outlook.com>
2025-12-03 15:11:52 +0700

    Updated - Model Dormitory

--- backend/src/models/dormitoryModel.js
+++ backend/src/models/dormitoryModel.js
@@ -1,2 +1,31 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../configs/database.js";
+
+const Dormitory = sequelize.define(
+  "dormitory",
+  {
+    dorm_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    dorm_name: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+    address: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    map: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+  },
+  {
+    freezeTableName: true,
+    timestamps: false,
+  }
+);
+
+export default Dormitory;
