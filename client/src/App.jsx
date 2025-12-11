cd07b046679946891a957a19c734dce2c2f4bd5f
WanchaiSae <Wanchai.sae@outlook.com>
2025-12-11 17:47:07 +0700

    Updated - Sidebar & Navbar Menu

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './component/Navbar'
+import Sidebar from './component/Sidebar'
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Sidebar />
     </div>
   )
 }
--- client/src/component/Navbar.jsx
+++ client/src/component/Navbar.jsx
@@ -1,8 +1,17 @@
-import React, {useState} from 'react'
+import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar Ohhh Yeahhh!!</div>
+    <nav className='px-6 py-4 bg-gray-900 text-white flex justify-between items-center'>
+      <div className='text-xl font-bold'>ระบบจัดการหอพัก</div>
+
+      <ul className='flex gap-6'>
+        <li className='cursor-pointer hover:text-gray-400'>หน้าหลัก</li>
+        <li className='cursor-pointer hover:text-gray-400'>เกี่ยวกับเรา</li>
+        <li className='cursor-pointer hover:text-gray-400'>คุณ วันชัย แซ่ลิ้ม</li>
+        <li className='cursor-pointer hover:text-gray-400'>ออกจากระบบ</li>
+      </ul>
+    </nav>
   )
   
 }
