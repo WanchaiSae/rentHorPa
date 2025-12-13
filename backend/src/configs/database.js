01ba2c9b20187bf9da0c3f357f8f344790f74a80
WanchaiSae <Wanchai.sae@outlook.com>
2025-12-13 16:33:19 +0700

    Added - Docker & Config

--- backend/src/configs/database.js
+++ backend/src/configs/database.js
@@ -5,10 +5,12 @@ const DB_HOST = process.env.DB_HOST || "localhost";
 const DB_NAME = process.env.DB_NAME || "database";
 const DB_USER = process.env.DB_USER || "user";
 const DB_PASSWORD = process.env.DB_PASSWORD || "";
+const DB_PORT = process.env.DB_PORT || 3306;
 const DB_DIALECT = process.env.DB_DIALECT || "mysql";
 
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
+  port: DB_PORT,
   dialect: DB_DIALECT,
 });
 
